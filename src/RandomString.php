25dcf1dcc8f0ecaeab16857c232e871d16826fc7
sfjp23j2 <tutbxaoptpo@gmail.com>
2024-03-05 21:04:43 +0330

    first pakage

---
+++ src/RandomString.php
@@ -0,0 +1,12 @@
+<?php
+namespace HelloWorldComposer;
+
+require_once 'vendor/autoload.php';
+
+class RandomString
+{
+    public static function helloWorld()
+    {
+        return 'Hello Composer!';
+    }
+}
